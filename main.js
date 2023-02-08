dfa0c4000cd8813b7148147e7362c5f078517aac
Stevixent <chiadikavirtue@gmail.com>
2023-02-08 22:42:54 +0100

    ok

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const typingText = document.querySelector(".typing-text");
 
 function randomParagraph() {
     //let randIndex = Math.floor(Math.random() * paragraphs.length);
-    console.log(Paragraphs[0]);
+    console.log(paragraphs[0]);
 }
 
 randomParagraph();
