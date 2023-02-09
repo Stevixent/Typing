22ad8b095b9801f134b76b2d3ad3f81f5df66a9c
Stevixent <chiadikavirtue@gmail.com>
2023-02-09 23:37:44 +0100

    ok

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ inpField = document.querySelector(".wrapper .input-field"),
 timeTag = document.querySelector(".time span b")
 mistakeTag = document.querySelector(".mistake span"),
 wpmTag = document.querySelector(".wpm span"),
-cpmTag = document.querySelector(".cpm span");
+cpmTag = document.querySelector(".cpm span"),
+tryAgainBtn = document.querySelector("button");
 
 let timer,
 maxTime = 60,
@@ -13,6 +14,7 @@ let charIndex =mistakes = isTyping = 0;
 function randomParagraph() {
     //getting random number and it'll always be less than the paragraphs length
     let randIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
     // getting random item from the paragraphs array
     //splitting all characters of it , adding each character inside a span , then adding this span in a p tag
     paragraphs[randIndex].split("").forEach(span => {
@@ -27,39 +29,46 @@ function randomParagraph() {
 function initTyping () {
     const characters = typingText.querySelectorAll("span");
     let typedChar = inpField.value.split("")[charIndex];
-    if (!isTyping) {
-        //once timer starts it wont restart again on every key clicked
-        timer = setInterval(initTimer, 1000);
-        isTyping = true;
-    }
-    // if user pressed backspace
-    if(typedChar == null) {
-        //decrease charIndex
-        charIndex--;
-        // reduce mistakes only if the charIndex span contain incorrect class
-        if (characters[charIndex].classList.contains("incorrect")) {
-           mistakes--; 
+    if (charIndex < characters.length - 1 && timeLeft > 0) {
+        if (!isTyping) {
+            //once timer starts it wont restart again on every key clicked
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
         }
-        characters[charIndex].classList.remove("correct","incorrect");
-    } else{
-
-        if (characters[charIndex].innerText === typedChar) {
-        // if user typed character and it matches add the correct class else add the incorrect class
-        //else increment the mistakes and add the incorrect class
-        characters[charIndex].classList.add("correct");
-    }else {
-        mistakes++;
-        characters[charIndex].classList.add("incorrect");
+        // if user pressed backspace
+        if(typedChar == null) {
+            //decrease charIndex
+            charIndex--;
+            // reduce mistakes only if the charIndex span contain incorrect class
+            if (characters[charIndex].classList.contains("incorrect")) {
+               mistakes--; 
+            }
+            characters[charIndex].classList.remove("correct","incorrect");
+        } else{
+    
+            if (characters[charIndex].innerText === typedChar) {
+            // if user typed character and it matches add the correct class else add the incorrect class
+            //else increment the mistakes and add the incorrect class
+            characters[charIndex].classList.add("correct");
+        }else {
+            mistakes++;
+            characters[charIndex].classList.add("incorrect");
+        }
+        charIndex++; //increment charIndex either user typed correct or incorrect characters
+        }
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
+    
+        let wpm = Math.round((((charIndex - mistakes) /5) / (maxTime - timeLeft)) * 60);
+        //if wpm value is 0, empty or infinity then set it's value to 0
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+        mistakeTag.innerText = mistakes;
+        wpmTag.innerText = wpm;
+        cpmTag.innerText = charIndex - mistakes; // cpm won't count mistakes
+    } else {
+        inpField.value = "";
+        clearInterval(timer);
     }
-    charIndex++; //increment charIndex either user typed correct or incorrect characters
-    }
-    characters.forEach(span => span.classList.remove("active"));
-    characters[charIndex].classList.add("active");
-
-    let wpm = Math.round((((charIndex - mistakes) /5) / ())
-    mistakeTag.innerText = mistakes;
-    cpmTag.innerText = charIndex - mistakes; // cpm won't count mistakes
-
 }
 
 function initTimer(){
@@ -72,5 +81,19 @@ function initTimer(){
     }
 }
 
+function resetGame() {
+    // calling paragraph function and resetting each variable and element value to default
+    randomParagraph();
+    inpField.value = "";
+    clearInterval(timer);
+    timeLeft = maxTime,
+    charIndex = mistakes = isTyping = 0;
+    timeTag.innerText = timeLeft;
+    mistakeTag.innerText = mistakes;
+    wpmTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
 randomParagraph();
 inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
